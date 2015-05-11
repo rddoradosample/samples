12bce7340c37e474f0c5eb6d012faa970e9ef0d2
rddoradosample <ronaldo@192-168-1-2.tpgi.com.au>
2015-05-11 23:11:16 +1000

    Project Setup
    Create Navigation Controller and TableView Controller

---
+++ dynamicheightcustomcell/dynamicheightcustomcell/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  
+//
+//  Created by Ronaldo II Dorado on 11/5/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
